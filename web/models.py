fa35ce4606aaee6d9137bcf01f0e62e8d77b19b4
unknown <mahdad@toman.local>
2021-03-25 17:15:41 +0430

    Expense model is tehre

--- web/models.py
+++ web/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+from django.db import models
+
+
 # Create your models here.
+class Expense(models.Model):
+    text = models.CharField(max_length=255, default=None)
+
+    date = models.DateTimeField(default=None)
+    amount = models.BigIntegerField(default=None)
+
+    user = models.ForeignKey(User,on_delete=models.CASCADE, default=None, null=True, blank=True)
+
