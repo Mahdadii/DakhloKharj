03bc41c0760cf11c9385c93ad65be084952adae6
unknown <mahdad@toman.local>
2021-03-26 14:20:05 +0430

    some beautification

--- web/models.py
+++ web/models.py
@@ -7,9 +7,19 @@ from django.db import models
 # Create your models here.
 class Expense(models.Model):
     text = models.CharField(max_length=255, default=None)
-
     date = models.DateTimeField(default=None)
     amount = models.BigIntegerField(default=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True, blank=True)
 
-    user = models.ForeignKey(User,on_delete=models.CASCADE, default=None, null=True, blank=True)
+    def __str__(self):
+        return '{}-{}'.format(self.date, self.amount)
 
+
+class Income(models.Model):
+    text = models.CharField(max_length=255, default=None)
+    date = models.DateTimeField(default=None)
+    amount = models.BigIntegerField(default=None)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, null=True, blank=True)
+
+    def __str__(self):
+        return '{}-{}'.format(self.date,self.amount)
